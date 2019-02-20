489bf80d32c8e38ff168fba0d7b2093e88fbae0e
yuichi-abe <biz11913@gmail.com>
2019-02-20 18:19:31 +0900

    初回コミット

---
+++ standard-layout/js/common.js
@@ -0,0 +1,16 @@
+
+
+// setInterval(function(){
+//     let now = new Date();
+//     console.log(now.getDay());
+//     let times = 0;
+//     console.log(`リロードします｡ : ${now}`);
+//     location.reload;
+// },10000);
+
+// let now = new Date();
+// console.log(now);
+// console.log(now.getDate());
+// console.log(now.getMonth(now));
+// console.log(now.getDay(now));
+
